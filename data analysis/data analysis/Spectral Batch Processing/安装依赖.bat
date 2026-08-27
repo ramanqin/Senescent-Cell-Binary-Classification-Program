5e28e0ccc95d8a8774bfd87cda19f39c946635c0
DZLL694 <3554458992@qq.com>
2026-08-27 15:49:12 +0800

    Delete data analysis/data analysis directory

--- data analysis/data analysis/Spectral Batch Processing/安装依赖.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0"
-python -m pip install -r requirements.txt
-echo.
-echo 依赖安装完成，可以双击“启动软件.bat”。
-pause
--- data analysis/data analysis/raman_spectrum_filter-main/__init__.py
+++
@@ -1,3 +0,0 @@
-__version__ = '1.6.0'
-
-from .parameter_finding import Filter_Data_Collect
